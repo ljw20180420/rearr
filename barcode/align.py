3299589e796ab0a9ffdfd2011dd31e87e47619b3
ljw <ljw2017@sjtu.edu.cn>
2023-09-21 16:51:45 +0800

    align to scaffold

--- barcode/align.py
+++ barcode/align.py
@@ -1,6 +1,7 @@
 #!/home/xiaoge/miniconda3/envs/crispr/bin/python
-import sys, os, subprocess
+import sys, os, subprocess, Bio.Seq
 barcodefile = sys.argv[1]
+scaffold = Bio.Seq.Seq("gttttagagctagaaatagcaagttaaaataaggctagtccgttatcaacttgaaaaagtggcaccgagtcggtgc").reverse_complement().__str__().upper()
 if not ".fq." in barcodefile and not ".fastq." in barcodefile:
     raise Exception("barcode file must contain fq or fastq")
 csvpos = barcodefile.find(".fq.") + 4 if ".fq." in barcodefile else barcodefile.find(".fastq.") + 7
@@ -23,7 +24,7 @@ with os.popen(f'''cut -f1-2,6,8 {barcodefile}''', "r") as bcd, os.popen(f'''perl
         qname, flag, chr, pos, _, CIGAR, _ = line.split("\t", 6)
         flag, pos, tglen = int(flag), int(pos), int(CIGAR[:-1])
         tgstrand = "-" if (flag // 16) % 2 else "+"
-        cut = pos + 16 + 6 if tgstrand == "+" else pos + tglen - 16 - 6
+        cut = pos - 1 + 16 + 6 if tgstrand == "+" else pos - 1 + tglen - 16 - 6
         rfstrand = "+" if tgstrand == "-" else "-" # the strand of target is constract to that of barcode and reference
         if rfstrand == "-":
             eul, edl, eur, edr = extdown_left, extup_left, extdown_right, extup_right
@@ -32,7 +33,7 @@ with os.popen(f'''cut -f1-2,6,8 {barcodefile}''', "r") as bcd, os.popen(f'''perl
         ref_left = subprocess.run(f'''bedtools getfasta -bedOut -s -fi /home/ljw/hg19_with_bowtie2_index/hg19.fa -bed <(echo "{chr}"$'\t'"{cut-eul}"$'\t'"{cut+edl}"$'\t'{qname}$'\t'"."$'\t'"{rfstrand}")''', shell=True, check=True, capture_output=True, executable="/bin/bash").stdout.decode().split()[-1].rstrip()
         ref_right = subprocess.run(f'''bedtools getfasta -bedOut -s -fi /home/ljw/hg19_with_bowtie2_index/hg19.fa -bed <(echo "{chr}"$'\t'"{cut-eur}"$'\t'"{cut+edr}"$'\t'{qname}$'\t'"."$'\t'"{rfstrand}")''', shell=True, check=True, capture_output=True, executable="/bin/bash").stdout.decode().split()[-1].rstrip()
         with open("barcode/reference", "w") as rd:
-            _ = rd.write(f"0\t0\t0\n{ref_left}\n{extup_left}\t{extup_left}\t{extup_left}\n{extup_right}\t{extup_right}\t{extup_right}\n{ref_right}\n{extup_right+extdown_right}\t{extup_right+extdown_right}\t{extup_right+extdown_right}\n")
+            _ = rd.write(f"0\t0\t0\n{ref_left}\n{extup_left}\t{extup_left}\t{extup_left}\n{extup_right}\t{extup_right}\t{extup_right}\n{ref_right}\n{extup_right+extdown_right}\t{extup_right+extdown_right}\t{extup_right+extdown_right}\n0\t0\t0\n{scaffold}\n{len(scaffold)}\t{len(scaffold)}\t{len(scaffold)}\n")
         
         while True:
             bcdline = bcd.readline()
