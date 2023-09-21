d834f614f36dddf56df06057b23dd199f63fc711
ljw <ljw2017@sjtu.edu.cn>
2023-09-21 15:50:52 +0800

    add run_all.sh

---
+++ barcode/run_all.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+while read fastqfile csvfile
+do
+    ( barcode/barcode.sh barcode/$fastqfile barcode/$csvfile; python barcode/align.py barcode/$fastqfile.$csvfile.barcode ) &
+done < <(echo "A2_TEST.fq" "final_hgsgrna_libb_all_0811-NGG.csv")
+wait
+
+# this is the example to run many fq csv pairs
+# while read fastqfile csvfile
+# do
+#     ( barcode/barcode.sh barcode/$fastqfile barcode/$csvfile; python barcode/align.py barcode/$fastqfile.$csvfile.barcode ) &
+# done < <(echo "A2_TEST.fq" "final_hgsgrna_libb_all_0811-NGG.csv"$'\n' \
+#             "fastq2.fq" "final_hgsgrna_libb_all_0811_NAA.csv")
+# wait
