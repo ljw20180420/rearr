84787c3b93338c89fb9468054cef64eb584ead63
ljw <ljw2017@sjtu.edu.cn>
2023-11-02 16:41:01 +0800

    extend_ref in pre_handle_sgRNA_PAM_ref.py now print warning if the reference cannot perfectly match hg19, or even worse, cannot map to hg19 by bowtie2

--- tools/pre_handle_sgRNA_PAM_ref.py
+++ tools/pre_handle_sgRNA_PAM_ref.py
@@ -1,12 +1,18 @@
 #!/usr/bin/env python
-import subprocess, Bio.Seq, Bio.Align, numpy, correct_micro_homology, os
+import subprocess, Bio.Seq, Bio.Align, numpy, os, re, sys, correct_micro_homology
 
 def extend_ref(ref, upstream_extend=0, downstream_extend=0):
     # extend ref to up/downstream if ref comes from genome
     if not upstream_extend and not downstream_extend:
         return ref
-    _, flag, chr, pos, _ = subprocess.run(f"echo {ref} | bowtie2 -x /home/ljw/hg19_with_bowtie2_index/hg19 -r -U - | samtools view", shell=True, executable="/bin/bash", check=True, capture_output=True).stdout.decode().split('\t', 4)
-    pos = int(pos) - 1
+    _, flag, chr, pos, left = subprocess.run(f"echo {ref} | bowtie2 -x /home/ljw/hg19_with_bowtie2_index/hg19 -r -U - | samtools view", shell=True, executable="/bin/bash", check=True, capture_output=True).stdout.decode().split('\t', 4)
+    if int(re.search("AS:i:(-?\d+)", left).group(1)) != 0:
+        sys.stderr.write("warning: the reference is not exactly match the genome\n")
+    try:
+        pos = int(pos) - 1
+    except Exception:
+        sys.stderr.write("the reference cannot be found in hg19\n")
+        return ref
     strand = "-" if (int(flag) / 16) % 2 else "+"
     if strand == "-":
         startup, endup, startdown, enddown = pos+len(ref), pos+len(ref)+upstream_extend, pos-downstream_extend, pos
