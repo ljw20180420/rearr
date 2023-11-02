f1f7da6823f0a02b02b5bf98c2029b2ff7deabac
ljw <ljw2017@sjtu.edu.cn>
2023-11-02 15:13:23 +0800

    install.sh is more clever, it does not add path to ~/.bashrc if ~/.bashrc already contains one

--- install.sh
+++ install.sh
@@ -2,9 +2,19 @@
 # simply append three paths to ~/.bashrc
 
 cd $(dirname $0)
-printf "\n%s\n" "export PATH=\"$(pwd)\":"'$PATH' >> ~/.bashrc
-printf "\n%s\n" "export PATH=\"$(pwd)/tools\":"'$PATH' >> ~/.bashrc
+echo $PATH
+if [[ $(grep -c $(pwd) ~/.bashrc) -eq 0 ]]
+then
+    printf "\n%s\n" "export PATH=\"$(pwd)\":"'$PATH' >> ~/.bashrc
+fi
+if [[ $(grep -c $(pwd)/tools ~/.bashrc) -eq 0 ]]
+then
+    printf "\n%s\n" "export PATH=\"$(pwd)/tools\":"'$PATH' >> ~/.bashrc
+fi
 cd Rearrangement
 cmake -DCMAKE_BUILD_TYPE=Release .
 make
-printf "\n%s\n" "export PATH=\"$(pwd)\":"'$PATH' >> ~/.bashrc
+if [[ $(grep -c $(pwd) ~/.bashrc) -eq 0 ]]
+then
+    printf "\n%s\n" "export PATH=\"$(pwd)\":"'$PATH' >> ~/.bashrc
+fi
