e4401302263ea7545cb5e9862475cd8e17b33d6b
ljw <ljw2017@sjtu.edu.cn>
2023-11-03 10:19:26 +0800

    uncomment the last half of install.sh

--- install.sh
+++ install.sh
@@ -26,21 +26,19 @@ else
     fi  
 fi
 
-
-
-# cd $(dirname $0)
-# if [[ $(grep -c $(pwd) ~/.bashrc) -eq 0 ]]
-# then
-#     printf "\n%s\n" "export PATH=\"$(pwd)\":"'$PATH' >> ~/.bashrc
-# fi
-# if [[ $(grep -c $(pwd)/tools ~/.bashrc) -eq 0 ]]
-# then
-#     printf "\n%s\n" "export PATH=\"$(pwd)/tools\":"'$PATH' >> ~/.bashrc
-# fi
-# cd Rearrangement
-# cmake -DCMAKE_BUILD_TYPE=Release .
-# make
-# if [[ $(grep -c $(pwd) ~/.bashrc) -eq 0 ]]
-# then
-#     printf "\n%s\n" "export PATH=\"$(pwd)\":"'$PATH' >> ~/.bashrc
-# fi
+cd $(dirname $0)
+if [[ $(grep -c $(pwd) ~/.bashrc) -eq 0 ]]
+then
+    printf "\n%s\n" "export PATH=\"$(pwd)\":"'$PATH' >> ~/.bashrc
+fi
+if [[ $(grep -c $(pwd)/tools ~/.bashrc) -eq 0 ]]
+then
+    printf "\n%s\n" "export PATH=\"$(pwd)/tools\":"'$PATH' >> ~/.bashrc
+fi
+cd Rearrangement
+cmake -DCMAKE_BUILD_TYPE=Release .
+make
+if [[ $(grep -c $(pwd) ~/.bashrc) -eq 0 ]]
+then
+    printf "\n%s\n" "export PATH=\"$(pwd)\":"'$PATH' >> ~/.bashrc
+fi
