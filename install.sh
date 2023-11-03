5565b4916d326a30287a6e823def830dfcdb6c4c
ljw <ljw2017@sjtu.edu.cn>
2023-11-03 10:18:18 +0800

    install.sh also install necessary python packages

--- install.sh
+++ install.sh
@@ -1,19 +1,46 @@
 #!/bin/bash
 # simply append three paths to ~/.bashrc
 
-cd $(dirname $0)
-if [[ $(grep -c $(pwd) ~/.bashrc) -eq 0 ]]
+echo "your conda environments are:"
+conda env list | sed '/^#/d' |sed '/^$/d' | cut -d" " -f1 | nl -w1
+env_list=$(conda env list | sed '/^#/d' |sed '/^$/d' | cut -d" " -f1)
+default_env=$(echo $env_list | cut -d" " -f1)
+read -p "which environment to use? (default: $default_env):" chosen_env
+if [[ $chosen_env == "" ]]
 then
-    printf "\n%s\n" "export PATH=\"$(pwd)\":"'$PATH' >> ~/.bashrc
+    echo "you do not choose, use the default env $default_env"
+    chosen_env=$default_env
+else
+    echo "you choose $chosen_env"
+    if [[ $env_list =~ $chosen_env ]]
+    then
+        conda install -n $chosen_env more-itertools biopython numpy
+    else
+        read -p "$chosen_env do not exist, do you want to create it? [y/n] (default: n):" create_env
+        case $create_env in
+            y|Y|yes|Yes|YES)
+                conda create -n $chosen_env more-itertools biopython numpy;;
+            *)
+                exit;;
+        esac
+    fi  
 fi
-if [[ $(grep -c $(pwd)/tools ~/.bashrc) -eq 0 ]]
-then
-    printf "\n%s\n" "export PATH=\"$(pwd)/tools\":"'$PATH' >> ~/.bashrc
-fi
-cd Rearrangement
-cmake -DCMAKE_BUILD_TYPE=Release .
-make
-if [[ $(grep -c $(pwd) ~/.bashrc) -eq 0 ]]
-then
-    printf "\n%s\n" "export PATH=\"$(pwd)\":"'$PATH' >> ~/.bashrc
-fi
+
+
+
+# cd $(dirname $0)
+# if [[ $(grep -c $(pwd) ~/.bashrc) -eq 0 ]]
+# then
+#     printf "\n%s\n" "export PATH=\"$(pwd)\":"'$PATH' >> ~/.bashrc
+# fi
+# if [[ $(grep -c $(pwd)/tools ~/.bashrc) -eq 0 ]]
+# then
+#     printf "\n%s\n" "export PATH=\"$(pwd)/tools\":"'$PATH' >> ~/.bashrc
+# fi
+# cd Rearrangement
+# cmake -DCMAKE_BUILD_TYPE=Release .
+# make
+# if [[ $(grep -c $(pwd) ~/.bashrc) -eq 0 ]]
+# then
+#     printf "\n%s\n" "export PATH=\"$(pwd)\":"'$PATH' >> ~/.bashrc
+# fi
