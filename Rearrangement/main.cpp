c74a8e039a5ee0c51b634bd6a651100295928978
ljw <ljw2017@sjtu.edu.cn>
2023-12-06 19:38:38 +0800

    read reference from 3<

--- Rearrangement/main.cpp
+++ Rearrangement/main.cpp
@@ -1,6 +1,8 @@
 #include "headers/parser.h"
 #include "headers/align.h"
 
+const static uint32_t longest_allowed_ref = 100000; 
+
 int main(int argc, char **argv)
 {
     print_help(argc, argv);
@@ -9,24 +11,40 @@ int main(int argc, char **argv)
     
     std::deque<std::string> refs;
     std::deque<uint32_t> upper_boundaries, down_boundaries;
-    std::ifstream fin(cc.ref_file);
-    uint32_t upper_boundary, down_boundary;
-    std::string ref;
     uint32_t max_ref_sz = 0;
-    while(fin >> upper_boundary >> ref >> down_boundary)
+    FILE *ref_fd = fdopen(3, "r");
+    char *buffer_ref = new char[longest_allowed_ref];
+    for (size_t i = 0; fgets(buffer_ref, longest_allowed_ref, ref_fd); ++i)
     {
-        std::transform(ref.begin(), ref.end(), ref.begin(), toupper);
-        max_ref_sz = ref.size() > max_ref_sz ? ref.size() : max_ref_sz;
-        refs.push_back(ref);
-        upper_boundaries.push_back(upper_boundary);
-        down_boundaries.push_back(down_boundary);
+        switch (i % 3)
+        {
+            case 0:
+                upper_boundaries.push_back(std::atoi(buffer_ref));
+                break;
+            case 1:
+                max_ref_sz = std::strlen(buffer_ref) > max_ref_sz ? std::strlen(buffer_ref) : max_ref_sz;
+                if (max_ref_sz + 1 == longest_allowed_ref)
+                {
+                    std::cerr << "reference buffer is not enough\n";
+                    return EXIT_FAILURE;
+                }
+                buffer_ref[strcspn(buffer_ref, "\n")] = '\0';
+                refs.push_back(buffer_ref);
+                std::transform(refs.back().begin(), refs.back().end(), refs.back().begin(), toupper);
+                break;
+            case 2:
+                down_boundaries.push_back(std::atoi(buffer_ref));
+                break;
+        }
     }
-    fin.close();
     if(!max_ref_sz)
     {
         std::cerr << "empty reference loaded\n";
         return EXIT_FAILURE;
     }
+    delete[] buffer_ref;
+    fclose(ref_fd);
+
     std::vector<int32_t> Es(max_ref_sz + 1), Gs(max_ref_sz + 1), Gps(max_ref_sz + 1), gr(max_ref_sz + 1);
     gr[0] = 0;
     gr[1] = cc.rv;
