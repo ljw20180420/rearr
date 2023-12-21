a0a2ed0dd633afc5e81c914a13d8b09495ec9ea3
ljw <ljw2017@sjtu.edu.cn>
2023-12-21 11:25:48 +0800

    add bench for reflen and probability, roll back to --std=c++17

--- bench/tools/compare_indel.py
+++ bench/tools/compare_indel.py
@@ -8,30 +8,40 @@ def NDsegment_distance(indel1, indel2, range1, range2):
     actual = range[1] if target > range[1] else range[0] if target < range[0] else target
     return indel1 + actual - indel2
 
-def get_range(indel, ref):
+def get_range(indel, ref1, ref2):
     range = [0, 0]
-    while indel[0] + range[0] - 1 >= 0 and indel[1] + range[0] - 1 >= 0 and ref[indel[0] + range[0] - 1] == ref[indel[1] + range[0] - 1]:
+    while indel[0] + range[0] - 1 >= 0 and indel[1] + range[0] - 1 >= 0 and ref1[indel[0] + range[0] - 1] == ref2[indel[1] + range[0] - 1]:
         range[0] = range[0] - 1
-    while indel[0] + range[1] < len(ref) and indel[1] + range[1] < len(ref) and ref[indel[0] + range[1]] == ref[indel[1] + range[1]]:
+    while indel[0] + range[1] < len(ref1) and indel[1] + range[1] < len(ref2) and ref1[indel[0] + range[1]] == ref2[indel[1] + range[1]]:
         range[1] = range[1] + 1
     return range
 
-def indel_dis(indel1, indel2, ref):
-    range1, range2 = get_range(indel1, ref), get_range(indel2, ref)
+def indel_dis(indel1, indel2, ref1, ref2):
+    range1, range2 = get_range(indel1, ref1, ref2), get_range(indel2, ref1, ref2)
     return NDsegment_distance(indel1, indel2, range1, range2)
 
-ref = sys.argv[1]
+_, ref1, ref2, randomseq = sys.argv
 
-for line in sys.stdin:
-    mindis = numpy.inf
-    query, refl1, refr1, queryl1, queryr1, c2 = line.split("\t", 5)
-    indel1 = numpy.array([int(refl1), int(refr1), int(queryl1), int(queryr1)])
-    c2 = c2.split("\t")
-    for i in range(0, len(c2), 4):
-        indel2 = numpy.array([int(c2[i]), int(c2[i + 1]), int(c2[i + 2]), int(c2[i + 3])])
-        disv = indel_dis(indel1, indel2, ref)
-        dis = numpy.sum(numpy.abs(disv))
-        if dis < mindis:
-            mindis = dis
-            mindisv = disv
-    sys.stdout.write(f"{query}\t{mindisv[0]}\t{mindisv[1]}\t{mindisv[2]}\t{mindisv[3]}\n")
+with open(randomseq, "r") as rd:
+    mode, reflen, probability, readnum, program, usertime, systime, realtime, memory = [None] * 9
+    for line in sys.stdin:
+        mode, reflen, probability, readnum, program, usertime, systime, realtime, memory, name, c2 = line.rstrip().split("\t", 10)
+        for randline in rd:
+            query, refl1, refr1, queryl1, queryr1 = randline.rstrip().split("\t")
+            if query == name:
+                break
+            sys.stdout.write(f"{mode}\t{reflen}\t{probability}\t{readnum}\t{program}\t{usertime}\t{systime}\t{realtime}\t{memory}\t{query}\t*\t*\t*\t*\n")
+        mindis = numpy.inf
+        indel1 = numpy.array([int(refl1), int(refr1), int(queryl1), int(queryr1)])
+        c2 = c2.split("\t")
+        for i in range(0, len(c2), 4):
+            indel2 = numpy.array([int(c2[i]), int(c2[i + 1]), int(c2[i + 2]), int(c2[i + 3])])
+            disv = indel_dis(indel1, indel2, ref1, ref2)
+            dis = numpy.sum(numpy.abs(disv))
+            if dis < mindis:
+                mindis = dis
+                minindel2 = indel2
+        sys.stdout.write(f"{mode}\t{reflen}\t{probability}\t{readnum}\t{program}\t{usertime}\t{systime}\t{realtime}\t{memory}\t{query}\t{minindel2[0]}\t{minindel2[1]}\t{minindel2[2]}\t{minindel2[3]}\n")
+    for randline in rd:
+        query, refl1, refr1, queryl1, queryr1 = randline.rstrip().split("\t")
+        sys.stdout.write(f"{mode}\t{reflen}\t{probability}\t{readnum}\t{program}\t{usertime}\t{systime}\t{realtime}\t{memory}\t{query}\t*\t*\t*\t*\n")
