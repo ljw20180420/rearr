2b75d31159537e690b2a2c295f384c0f4f6ce671
ljw <ljw2017@sjtu.edu.cn>
2024-01-02 16:51:21 +0800

    add trouble_shooting.r to calculate insertion percentage at -4

---
+++ barcode/tools/trouble_shooting.r
@@ -0,0 +1,59 @@
+#!/usr/bin/env Rscript
+
+library(tidyverse)
+
+# wt1 <- read_tsv("barcode/sxanalysis/D2-wt1-g1n-1.csv.wfilter", col_names = c("sgRNA", "del", "indel", "ins", "1bp", "<2bp", "<3bp", "<4bp", "<5bp", "<6bp", "<7bp", "<8bp", "<9bp", "<10bp")) |>
+#   mutate(sample = "wt1")
+# wt2 <- read_tsv("barcode/sxanalysis/D2-wt2-g1n-1.csv.wfilter", col_names = c("sgRNA", "del", "indel", "ins", "1bp", "<2bp", "<3bp", "<4bp", "<5bp", "<6bp", "<7bp", "<8bp", "<9bp", "<10bp")) |>
+#   mutate(sample = "wt2")
+# bind_rows(wt1, wt2) |>
+#   mutate(minus4 = factor(substring(sgRNA, 17, 17), levels = c("A", "T", "C", "G"))) |>
+#   filter(del >= 0 & del <= 1 & indel >= 0 & indel <= 1 & ins >= 0 & ins <= 1) |>
+#   unite(sample4, sample, minus4, remove = FALSE) |>
+#   ggplot(aes(minus4, indel + ins)) +
+#   stat_summary(fun = "mean") +
+#   scale_y_continuous(labels = scales::percent) -> ggfig
+# ggsave("percentage.sx.png", path = "barcode/sxanalysis", width = 22, height = 12)
+
+#######################################################
+# Usage: trouble_shooting.r
+#######################################################
+args <- commandArgs(trailingOnly = TRUE)
+fqfile <- args[1]
+
+csvfile <- case_match(
+  str_to_upper(substring(strsplit(fqfile, "-")[[1]][2], 1, 2)),
+  "A1" ~ "final_hgsgrna_libb_all_0811_NAA_scaffold_nbt_A1.csv",
+  "A2" ~ "final_hgsgrna_libb_all_0811_NAA_scaffold_nbt_A2.csv",
+  "A3" ~ "final_hgsgrna_libb_all_0811_NAA_scaffold_nbt_A3.csv",
+  "G1" ~ "final_hgsgrna_libb_all_0811_NGG_scaffold_nor_G1.csv",
+  "G2" ~ "final_hgsgrna_libb_all_0811_NGG_scaffold_nor_G2.csv",
+  "G3" ~ "final_hgsgrna_libb_all_0811_NGG_scaffold_nor_G3.csv",
+)
+
+# read barcode and sgRNA
+barcode_sgRNA <- read_csv(pipe(sprintf("rev barcode/csvfiles/%s | cut -c23-40 | tr 'ACGT' 'TGCA'", csvfile)), col_names = "barcode") |>
+  bind_cols(read_csv(pipe(sprintf("sed -r 's/[acgt]+.*$//' barcode/csvfiles/%s | rev | cut -c1-20 | rev", csvfile)), col_names = "sgRNA"))
+
+# load table
+idtable <- read_tsv(sprintf("%s.table", fqfile))
+
+# all insertions
+idtable |>
+  left_join(barcode_sgRNA, by = "barcode") |>
+  mutate(minus4 = factor(substring(sgRNA, 17, 17), levels = c("A", "T", "C", "G"))) |>
+  mutate(ins = factor(ref_end1 > cut1 | !is.na(random_insertion) | ref_start2 < cut2, levels = c(FALSE, TRUE), labels = c("not insertion", "insertion"))) |>
+  ggplot(aes(minus4, fill = ins, weight = count)) +
+  geom_bar(position = "fill") +
+  scale_y_continuous(labels = scales::percent) -> ggfig
+ggsave(sprintf("%s.insertion.png", basename(fqfile)), path = dirname(fqfile), width = 22, height = 12)
+
+# templated insertions
+idtable |>
+  left_join(barcode_sgRNA, by = "barcode") |>
+  mutate(minus4 = factor(substring(sgRNA, 17, 17), levels = c("A", "T", "C", "G"))) |>
+  mutate(ins = factor(ref_end1 > cut1 | ref_start2 < cut2, levels = c(FALSE, TRUE), labels = c("not insertion", "insertion"))) |>
+  ggplot(aes(minus4, fill = ins, weight = count)) +
+  geom_bar(position = "fill") +
+  scale_y_continuous(labels = scales::percent) -> ggfig
+ggsave(sprintf("%s.templated_insertion.png", basename(fqfile)), path = dirname(fqfile), width = 22, height = 12)
