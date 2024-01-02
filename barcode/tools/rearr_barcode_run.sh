9912a21f55b7e29efdb764392f2f9c8e8020304d
ljw <ljw2017@sjtu.edu.cn>
2024-01-02 13:50:22 +0800

    R1 sgRNA match R2 barcode

--- barcode/tools/rearr_barcode_run.sh
+++ barcode/tools/rearr_barcode_run.sh
@@ -1,32 +1,105 @@
 #!/bin/bash
 
+infer_csvfile()
+{
+    chip=$(cut -d'-' -f2 <<<$1 | head -c2)
+    case ${chip^^} in
+        G?)
+            echo "barcode/csvfiles/final_hgsgrna_libb_all_0811_NGG_scaffold_nor_${chip^^}.csv"
+            ;;
+        A?)
+            echo "barcode/csvfiles/final_hgsgrna_libb_all_0811_NAA_scaffold_nbt_${chip^^}.csv"
+            ;;
+        *)
+            echo "cannot infer csv file from fastq file" >&2
+            exit
+            ;;
+    esac
+}
+
+
+
 find_barcode()
 {
-    primer="TCAAGACCTAGCTAGCGAATT"
-    minscore=30
-    fastqfile=$1
-    csvfile=$2
+    local minscore=30
+    local fq1=$1
+    local csvfile=$2
+    local bowtie2genome=$3
+    local getfastagenome=$4
+    local fq2=${fq1%.*}.R2.${fq1##*.}
 
-    sed -n '2~4p' "$fastqfile" | sort | uniq -c | awk -v OFS="\t" '{print $2, $1}' >"$fastqfile.count"
-    cut -f1 "$fastqfile.count" | bowtie2 --quiet --local -L 15 --ma 1 --mp 2,2 --rdg 3,1 --rfg 3,1 --score-min C,$minscore -r -x "$csvfile.primer+barcode" -U - 2>/dev/null | samtools view | cut -f2,3,6 | $(which python) -c '
+    sed -n '2~4p' "$fq2" | paste - <(sed -n '2~4p' "$fq1") | sort | uniq -c | awk -v OFS="\t" '{print $2, $3, $1}' >"$fq1.count"
+    
+    
+
+    cut -f1 "$fq1.count" | bowtie2 --quiet --norc --mm --local -L 15 --ma 1 --mp 2,2 --rdg 3,1 --rfg 3,1 --score-min C,$minscore -r -x "$csvfile.primer+barcode" -U - 2>/dev/null | samtools view | cut -f2,3,6 | $(which python) -c '
 import sys, pysam
 for line in sys.stdin:
     flag, barcode, CIGAR = line.split("\t")
     pAS = pysam.AlignedSegment()
     pAS.cigarstring = CIGAR
     sys.stdout.write(f"{flag}\t{barcode}\t{pAS.query_alignment_end}\n")
-    ' | awk -F "\t" -v csvfile=$csvfile -v primer=$primer -v OFS="\t" 'BEGIN{pbfa = csvfile ".primer+barcode.fa"; while (getline bn <pbfa) {getline bc <pbfa; sub("^" primer, "", bc); bn2bc[substr(bn, 2)] = bc}} {print $1, bn2bc[$2], $3}' | paste "$fastqfile.count" <(cut -f1 "$fastqfile.count" | sed '=' | sed '1~2s/^/>s/' | cutadapt -a GCACCGACTCGGTGCCACTTTTTCAAGTTGATAACGGACTAGCCTTATTTTAACTTGCTATTTCTAGCTCTAAAAC - 2> /dev/null | sed '1~2d') - | awk -F "\t" -v OFS="\t" -v minscore=$minscore '{if (($4/4)%2==1 || ($4/16)%2==1) print $1, $2 > "'"$fastqfile.$(basename $csvfile).not_find"'"; else if ($6+minscore>length($3)) print $1, $2 > "'"$fastqfile.$(basename $csvfile).too_short"'"; else print $1, $2, $3, $6, $5}' | sort -k5,5 -k2,2nr >"$fastqfile.$(basename $csvfile).barcode"
+    ' | awk -F "\t" -v csvfile="$csvfile" -v OFS="\t" '
+        BEGIN{
+            pbfa = csvfile ".primer+barcode.fa"; 
+            ref12 = csvfile ".ref12"
+            while (getline bn <pbfa) 
+            {
+                getline bc <pbfa;
+                bc = substr(bc, 22)
+                bn2bc[substr(bn, 2)] = bc;
+                getline r12 <ref12;
+                bn2r12[substr(bn, 2)] = r12
+            }
+        }
+        {
+            print $1, $2, $3, bn2bc[$2], bn2r12[$2]
+        }
+    ' | paste "$fq1.count" <(cut -f1 "$fq1.count" | sed '=' | sed '1~2s/^/>s/' | cutadapt -a GCACCGACTCGGTGCCACTTTTTCAAGTTGATAACGGACTAGCCTTATTTTAACTTGCTATTTCTAGCTCTAAAAC - 2> /dev/null | sed '1~2d') - <(cut -f2 "$fq1.count" | bowtie2 --quiet --norc --mm --local -L 15 --ma 1 --mp 2,2 --rdg 3,1 --rfg 3,1 --score-min C,$minscore -r -x "$csvfile.sgRNA+scaffold" -U - 2>/dev/null | samtools view | cut -f2,3) | awk -F "\t" -v OFS="\t" -v minscore=$minscore -v fq1="$fq1" '
+    {
+        if (($5/4)%2 == 1 || ($11/4)%2 == 1 || $6 != $12 || $7 + minscore > length($4))
+            print $0 > fq1".not_find";
+        else
+            print $1, $3, $4, $7, $8, $9, $10;
+    }
+    ' | awk -F "\t" -v OFS="\t" '
+    {
+        if ($1 != read2)
+        {
+            if (NR > 1)
+                print read2, count, read2rmadapter, query_start, barcode, ref1, ref2;
+            read2 = $1;
+            count = $2;
+            read2rmadapter = $3;
+            query_start = $4;
+            barcode = $5;
+            ref1 = $6;
+            ref2 = $7
+        }
+        else
+            count += $2;
+    }
+    END{
+        print read2, count, read2rmadapter, query_start, barcode, ref1, ref2;
+    }
+    ' | sort -k5,5 -k2,2nr
 }
 
 bowtie2genome=$1
 getfastagenome=$2
+ext1up=50
+ext2up=10
 
-while read fastqfile csvfile
+while read fq1
 do
-    ( find_barcode "$fastqfile" "$csvfile"; rearr_barcode_align.py "$fastqfile.$(basename $csvfile).barcode" "$csvfile" "$bowtie2genome" "$getfastagenome" ) &
+    csvfile=$(infer_csvfile "$fq1")
+    find_barcode "$fq1" "$csvfile" "$bowtie2genome" "$getfastagenome" | rearr_barcode_align.py "$fq1" "$ext1up" "$ext2up" &
 done
 
 jobs
 wait
+
+
+
 
 
--- barcode/tools/rearr_index_barcode.sh
+++ barcode/tools/rearr_index_barcode.sh
@@ -1,7 +1,78 @@
 #!/bin/bash
 
-primer="TCAAGACCTAGCTAGCGAATT"
-csvfile=$1
+get_reference()
+{
+    local csvfile=$1
+    local bowtie2genome=$2
+    local getfastagenome=$3
+    local ext1up=$4
+    local ext1down=$5
+    local ext2up=$6
+    local ext2down=$7
+    perl -anF, -E '
+        $rev = scalar reverse $F[1];
+        $rev=~m/[acgt]/g;
+        $target=substr($F[1], length($F[1]) - pos($rev) + 1, 44);
+        substr($target, 16, 2)="CC";
+        say $target;
+    ' "$csvfile" | bowtie2 --quiet --mm -x "$bowtie2genome" -r -U - 2> /dev/null | samtools view | awk -F "\t" -v OFS="\t" -v ext1up="$ext1up" -v ext1down="$ext1down" -v ext2up="$ext2up" -v ext2down="$ext2down" '
+        {
+            qname = $1
+            flag = $2
+            if (int(flag / 16) % 2)
+                refstrand = "+"
+            else
+                refstrand = "-"
+            chr = $3
+            pos = $4 - 1
+            if (refstrand == "+")
+            {
+                target_length = substr($6, 1, length($6) - 1)
+                cut = pos + target_length - 16 - 6
+                print chr, cut - ext1up, cut + ext1down, qname, ".", "+"
+                print chr, cut - ext2up, cut + ext2down, qname, ".", "+"
+            }
+            else
+            {
+                cut = pos + 16 + 6
+                print chr, cut - ext1down, cut + ext1up, qname, ".", "-"
+                print chr, cut - ext2down, cut + ext2up, qname, ".", "-"
+            }
+        }
+    ' | bedtools getfasta -s -fi "$getfastagenome" -bed - | sed '1~2d' | perl -snE '
+        if ($AG eq "A"){
+            if ($. % 2){
+                $spos = $ext1up + 4
+            }
+            else{
+                $spos = $ext2up + 4
+            }
+            substr($_, $spos, 2) = "AA"
+        }
+        if ($. % 2){
+            chomp;
+            print $_
+        }
+        else{
+            print "\t" . $_
+        }
+    ' -- -ext1up="$ext1up" -ext2up="$ext2up" -AG="${csvfile: -6:1}"
+}
 
-tail -n+2 $csvfile | cut -d',' -f12 | tr ACGT TGCA | rev | sort | sed '=' | sed '1~2s/^/>BC_/; 2~2s/^/'"$primer"'/' >"$csvfile.primer+barcode.fa"
-bowtie2-build -q "$csvfile.primer+barcode.fa" "$csvfile.primer+barcode"
+bowtie2genome=$1
+getfastagenome=$2
+ext1up=50
+ext1down=10
+ext2up=10
+ext2down=100
+
+for csvfile in final_hgsgrna_libb_all_0811_NAA_scaffold_nbt_A1.csv final_hgsgrna_libb_all_0811_NAA_scaffold_nbt_A2.csv final_hgsgrna_libb_all_0811_NAA_scaffold_nbt_A3.csv final_hgsgrna_libb_all_0811_NGG_scaffold_nor_G1.csv final_hgsgrna_libb_all_0811_NGG_scaffold_nor_G2.csv final_hgsgrna_libb_all_0811_NGG_scaffold_nor_G3.csv
+do
+    perl -anF, -E '$pribar=substr($F[1], length($F[1]) - 41, 39); $pribar=~tr/ACGT/TGCA/; say ">BC_" . $F[0], "\n", scalar reverse $pribar' barcode/csvfiles/$csvfile >"barcode/csvfiles/$csvfile.primer+barcode.fa"
+    bowtie2-build -q "barcode/csvfiles/$csvfile.primer+barcode.fa" "barcode/csvfiles/$csvfile.primer+barcode"
+
+    perl -anF, -E '$rev=scalar reverse $F[1]; $rev=~m/[acgt]/g; say ">BC_" . $F[0], "\n", substr($F[1], 0, length($F[1]) - pos($rev) + 1)' barcode/csvfiles/final_hgsgrna_libb_all_0811_NAA_scaffold_nbt_A1.csv >"barcode/csvfiles/$csvfile.sgRNA+scaffold.fa"
+    bowtie2-build -q "barcode/csvfiles/$csvfile.sgRNA+scaffold.fa" "barcode/csvfiles/$csvfile.sgRNA+scaffold"
+
+    get_reference "barcode/csvfiles/$csvfile" "$bowtie2genome" "$getfastagenome" $ext1up $ext1down $ext2up $ext2down >"barcode/csvfiles/$csvfile.ref12"
+done
