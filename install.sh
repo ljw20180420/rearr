91960f326311dc684ad7d8dd79b865d72960571c
ljw <ljw2017@sjtu.edu.cn>
2024-01-03 11:27:34 +0800

    install.sh now compile kpLogo, more_itertools and plotnine are not necessary python packages

--- install.sh
+++ install.sh
@@ -1,27 +1,33 @@
 #!/bin/bash
 
-cd $(dirname $0)
+cd "$(dirname "$0")" || exit
 
 # install python packages
 pip install -r requirements.txt
 
+# compile kpLogo for sgRNA analysis
+make -C barcode/kpLogo/src
+
 # simply append three paths to ~/.bashrc
-if [[ $(grep -c $(pwd) ~/.bashrc) -eq 0 ]]
+if [[ $(grep -c "$(pwd)" ~/.bashrc) -eq 0 ]]
 then
     printf "\n%s\n" "export PATH=\"$(pwd)\":"'$PATH' >> ~/.bashrc
 fi
-if [[ $(grep -c $(pwd)/tools ~/.bashrc) -eq 0 ]]
+
+if [[ $(grep -c "$(pwd)"/tools ~/.bashrc) -eq 0 ]]
 then
     printf "\n%s\n" "export PATH=\"$(pwd)/tools\":"'$PATH' >> ~/.bashrc
 fi
-if [[ $(grep -c $(pwd)/barcode/tools ~/.bashrc) -eq 0 ]]
+
+if [[ $(grep -c "$(pwd)"/barcode/tools ~/.bashrc) -eq 0 ]]
 then
     printf "\n%s\n" "export PATH=\"$(pwd)/barcode/tools\":"'$PATH' >> ~/.bashrc
 fi
+
 mkdir -p Rearrangement/build
 cmake -DCMAKE_BUILD_TYPE=Release -S Rearrangement -B Rearrangement/build
 make -C Rearrangement/build
-if [[ $(grep -c $(pwd)/Rearrangement/build ~/.bashrc) -eq 0 ]]
+if [[ $(grep -c "$(pwd)/Rearrangement/build" ~/.bashrc) -eq 0 ]]
 then
     printf "\n%s\n" "export PATH=\"$(pwd)/Rearrangement/build\":"'$PATH' >> ~/.bashrc
 fi
