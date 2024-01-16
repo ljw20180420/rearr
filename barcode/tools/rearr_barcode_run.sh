8722f23bfb7d3f26861a0b246c72edfd0fbf8bdd
ljw <ljw2017@sjtu.edu.cn>
2024-01-16 10:40:48 +0800

    rearr_barcode_run.sh now use infer_csvfile.sh

--- barcode/tools/rearr_barcode_run.sh
+++ barcode/tools/rearr_barcode_run.sh
@@ -1,24 +1,5 @@
 #!/bin/bash
 
-infer_csvfile()
-{
-    chip=$(awk -F "-" '{print $(NF - 1)}' <<<$1 | head -c2)
-    case ${chip^^} in
-        G?)
-            echo "barcode/csvfiles/final_hgsgrna_libb_all_0811_NGG_scaffold_nor_${chip^^}.csv"
-            ;;
-        A?)
-            echo "barcode/csvfiles/final_hgsgrna_libb_all_0811_NAA_scaffold_nbt_${chip^^}.csv"
-            ;;
-        *)
-            echo "cannot infer csv file from fastq file" >&2
-            exit
-            ;;
-    esac
-}
-
-
-
 find_barcode()
 {
     local minscoreR2=30
