644e9974d0c483aefdb8fd4862b7d093643c0cde
ljw <ljw2017@sjtu.edu.cn>
2024-01-18 15:53:28 +0800

    add usage of rearr_render.sh to README and rearr_render.sh itself

--- rearr_render.sh
+++ rearr_render.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+# Usage
+# rearr_render.sh input.alg.cut1.cut2.ref1len
+
 algfile="$(realpath $1)"
 quarto render "$(dirname $0)/tools/draw_figures.qmd" -P "algfile:$algfile"
 mv "$(dirname $0)/tools/draw_figures.html" "$algfile.html"
