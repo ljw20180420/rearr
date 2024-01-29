dba7ad13d7cd1a840e231070bc40500d5111cda1
ljw <ljw2017@sjtu.edu.cn>
2024-01-29 14:46:58 +0800

    give up quarto, use rmarkdown directly, add web interface

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 #install dependencies
-sudo apt-get update
-sudo apt-get upgrade
+sudo apt-get -y update && \
+sudo apt-get -y upgrade && \
 sudo apt-get -y install libcairo2-dev libtiff-dev fftw3-dev libharfbuzz-dev libfribidi-dev
 
 project_path="$(dirname $(realpath $0))"
@@ -10,10 +10,10 @@ project_path="$(dirname $(realpath $0))"
 cd "$project_path" || exit
 
 # install R packages
-Rscript -e '
-args=commandArgs(trailingOnly = TRUE)
-renv::use_python(python=args[1])
-renv::restore()
+Rscript --vanilla -e '
+    args = commandArgs(trailingOnly = TRUE)
+    renv::use_python(python = args[1])
+    renv::restore()
 ' "$(which python3.10)"
 
 # compile kpLogo for sgRNA analysis
@@ -35,3 +35,4 @@ cmake -DCMAKE_BUILD_TYPE=Release -S Rearrangement -B Rearrangement/build
 make -C Rearrangement/build
 
 sudo ln -sf "$project_path/rearr_web.sh" /usr/local/bin
+sudo ln -sf "$(which Rscript)" $project_path
