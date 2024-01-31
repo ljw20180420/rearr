ed5a9a57fedbb5f61beb9d8c265205831cc2280a
ljw <ljw2017@sjtu.edu.cn>
2024-01-31 16:07:57 +0800

    restore run_kpLogo.sh

--- barcode/tools/run_kpLogo.sh
+++ barcode/tools/run_kpLogo.sh
@@ -9,24 +9,23 @@ csvfile="$($project_path/barcode/tools/infer_csvfile.sh $fq1)"
 method=$2
 threshold=${3:-40}
 
-
 $project_path/barcode/tools/rearr_barcode_post_process.sh <"$fq1.table" | tail -n+2 | join -t $'\t' -1 1 -2 1 - <(rev "$csvfile" | cut -c23-40 | tr 'ACGT' 'TGCA' | paste - <(sed -r 's/[acgt]+.*$//' "$csvfile" | rev | cut -c1-20 | rev) | sort) | awk -F "\t" -v OFS="\t" -v fq1="$fq1" -v threshold="$threshold" '
     {
-        if ($24 != sgRNA && sgRNA)
+        if ($25 != sgRNA && sgRNA)
         {
             for (i = 0; i < inscount; ++i)
                 print sgRNA
             for (i = 0; i < count; ++i)
                 print sgRNA > fq1 ".kpLogo.total"
-            print sgRNA, inscount / count, inscount, count > fq1 ".kpLogo.weight"
+            print sgRNA, inscount / count, inscount + 0, count > fq1 ".kpLogo.weight"
             if (count > threshold)
-                print sgRNA, inscount / count, inscount, count > fq1 ".kpLogo.weight.thres"
+                print sgRNA, inscount / count, inscount + 0, count > fq1 ".kpLogo.weight.thres"
             count = 0
             inscount = 0
         }
-        sgRNA = $24
+        sgRNA = $25
         count += $3
-        if ($23 == "ins" || $23== "indel")
+        if ($24 == "ins" || $24== "indel")
             inscount += $3
     }
     END{
@@ -34,9 +33,9 @@ $project_path/barcode/tools/rearr_barcode_post_process.sh <"$fq1.table" | tail -
             print sgRNA
         for (i = 0; i < count; ++i)
             print sgRNA > fq1 ".kpLogo.total"
-        print sgRNA, inscount / count, inscount, count > fq1 ".kpLogo.weight"
+        print sgRNA, inscount / count, inscount + 0, count > fq1 ".kpLogo.weight"
         if (count > threshold)
-            print sgRNA, inscount / count, inscount, count > fq1 ".kpLogo.weight.thres"
+            print sgRNA, inscount / count, inscount + 0, count > fq1 ".kpLogo.weight.thres"
     }
 ' >"$fq1.kpLogo.insertion"
 
