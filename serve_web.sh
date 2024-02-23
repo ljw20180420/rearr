85601d2aa323be22b18f23409e3cba9c1e6ce892
ljw <ljw2017@sjtu.edu.cn>
2024-02-23 09:05:46 +0800

    remove flask debug mode

--- serve_web.sh
+++ serve_web.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 project_path="$(dirname $(realpath $0))"
-$project_path/py312/bin/python3.12 -m flask --app $project_path/web/rearr_web.py run --debug -h localhost -p 8000
+# $project_path/py312/bin/python3.12 -m flask --app $project_path/web/rearr_web.py run --debug -h localhost -p 8000
+$project_path/py312/bin/python3.12 -m flask --app $project_path/web/rearr_web.py run -h localhost -p 8000
