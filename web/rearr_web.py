445591d9028cff0f502327982040542a5b62f6a0
ljw <ljw2017@sjtu.edu.cn>
2024-02-29 10:41:40 +0800

    guard program from repeat running

--- web/rearr_web.py
+++ web/rearr_web.py
@@ -1,4 +1,5 @@
-import os, shutil, subprocess
+import os, shutil, subprocess, sys
+import concurrent.futures
 from flask import Flask, render_template, request, send_file, jsonify
 from werkzeug.utils import secure_filename
 # from flask_autoindex import AutoIndex
@@ -14,6 +15,11 @@ class CustomFlask(Flask):
         comment_start_string='{c{s',
         comment_end_string='c}e}',
     ))
+
+# cpulimit = max(os.cpu_count() * 3 // 4, 2)
+cpulimit = 1
+executor = concurrent.futures.ThreadPoolExecutor(max_workers=cpulimit)
+sep = "--xy9b92jiKcrG--"
 
 app = CustomFlask(__name__)
 # AutoIndex(app)   
@@ -34,11 +40,24 @@ def uploadFile():
         return str(err)
     
 def displayDir(links, rootpath, subpath):
+    runnings = os.listdir(os.path.join(app.root_path, "running_query"))
+    for running in runnings: # debug
+        sys.stderr.write(running + "\n") # debug
     for fsterm in os.listdir(os.path.join(rootpath, "users", subpath)):
         if os.path.isdir(os.path.join(rootpath, "users", subpath, fsterm)):
             links[fsterm + "/"] = os.path.join("/users", subpath, fsterm)
         else:
-            links[fsterm] = os.path.join("/users", subpath, fsterm)
+            if fsterm.endswith(".fq.gz") or fsterm.endswith(".fastq.gz") or fsterm.endswith(".fq") or fsterm.endswith(".fastq"):
+                runname = os.path.join("users", subpath, fsterm).replace("/", ".")
+                state = "new"
+                for qstate in ["running", "wait", "finish", "fail"]:
+                    sys.stderr.write(f"{runname}.{qstate}\n") # debug
+                    if f"{runname}.{qstate}" in runnings:
+                        state = qstate
+                        break
+                links[fsterm] = os.path.join("/users", subpath, fsterm) + sep + state
+            else:
+                links[fsterm] = os.path.join("/users", subpath, fsterm)
     return jsonify(links)
 
 @app.route('/users')
@@ -77,7 +96,42 @@ def createSymlink():
         return "success"
     except Exception as err:
         return str(err)
-    
+
+def start_run(runname, fastqfile, cmd):
+    os.remove(os.path.join(app.root_path, "running_query", f"{runname}.wait"))
+    with open(os.path.join(app.root_path, "running_query", f"{runname}.running"), "w") as fd:
+        pass
+    try:
+        with open(f"{fastqfile}.stdout", "w") as sdo, open(f"{fastqfile}.stderr", "w") as sde:
+            _ = subprocess.run(cmd, stdout = sdo, stderr = sde, shell=True, executable="/bin/bash")
+        with open(os.path.join(app.root_path, "running_query", f"{runname}.finish"), "w") as fd:
+            pass
+    except:
+        with open(os.path.join(app.root_path, "running_query", f"{runname}.fail"), "w") as fd:
+            pass
+    os.remove(os.path.join(app.root_path, "running_query", f"{runname}.running"))
+
+def run_controller(subpath, fastqfile, cmd):
+    try:
+        runname = subpath.replace("/",".")
+        runnings = os.listdir(os.path.join(app.root_path, "running_query"))
+        if f"{runname}.running" in runnings:
+            raise Exception("job is already running")
+        elif f"{runname}.wait" in runnings:
+            raise Exception("job is already in query")
+        else:
+            for qstate in ["fail", "finish"]:
+                if f"{runname}.{qstate}" in runnings:
+                    os.remove(os.path.join(app.root_path, "running_query", f"{runname}.{qstate}"))
+        with open(os.path.join(app.root_path, "running_query", f"{runname}.wait"), "w") as fd:
+            pass
+        executor.submit(start_run, runname, fastqfile, cmd)
+        return "your job is added to queue"
+    except Exception as err:
+        with open(os.path.join(app.root_path, "running_query", f"{runname}.fail"), "w") as fd:
+            pass
+        raise err
+
 @app.route('/run/<path:subpath>', methods = ["PUT"])
 def runRearr(subpath):
     try:
@@ -85,11 +139,10 @@ def runRearr(subpath):
         fastqfile = os.path.join(app.root_path, subpath)
         ref1, ref2, cut1, cut2, NGGCCNtype1, NGGCCNtype2 = request.form['ref1'], request.form['ref2'], request.form['cut1'], request.form['cut1'], request.form['NGGCCNtype1'], request.form['NGGCCNtype2']
         cmd = f'''{os.path.join(project_path, "rearr_run.sh")} {fastqfile} {ref1} {ref2} {cut1} {cut2} {NGGCCNtype1} {NGGCCNtype2}'''
-        with open(f"{fastqfile}.stdout", "w") as sdo, open(f"{fastqfile}.stderr", "w") as sde:
-            results = subprocess.run(cmd, stdout = sdo, stderr = sde, shell=True, executable="/bin/bash")
+        results = run_controller(subpath, fastqfile, cmd)
         return str(results)
     except Exception as err:
         return str(err)
     
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
