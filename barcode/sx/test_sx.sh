e2e52786bac7d2fd023afbb0b8bf5db7d627b935
ljw <ljw2017@sjtu.edu.cn>
2024-03-22 19:23:55 +0800

    the initial version of docker deployment

--- barcode/sx/test_sx.sh
+++ barcode/sx/test_sx.sh
@@ -7,11 +7,11 @@ spliter1="${csvfile}.adapter+sgRNA+scaffold"
 spliter2="${csvfile}.primer+barcode"
 sgRNAfile="${csvfile}.sgRNA"
 ref12="${csvfile}.ref12"
-# read -ep "path to genome reference:" genomeref
+read -ep "path to genome reference:" genomeref
 
-# $project_path/barcode/sx/index_spliter.sh $genomeref
-# $project_path/barcode/tools/demultiplex.sh "${fqR1}" "${fqR2}" "${spliter1}" "${spliter2}" "${sgRNAfile}" 100 30 $ref12 30 >"${fqR1}.demultiplex"
-# $project_path/barcode/tools/barcode_align.sh "${fqR1}" 50 10 $(cat "${fqR1}.total") <"${fqR1}.demultiplex" >"${fqR1}.alg" 3>"${fqR1}.table"
-# "${project_path}/barcode/tools/run_kpLogo.sh" "$project_path/barcode/test/A2-g1n-3.fq" weight
-# "${project_path}/barcode/tools/trouble_shooting.r" "${fqR1}" -9999
+$project_path/barcode/sx/index_spliter.sh $genomeref $(ls ${project_path}/barcode/sx/csvfiles/*.csv)
+$project_path/barcode/tools/demultiplex.sh "${fqR1}" "${fqR2}" "${spliter1}" "${spliter2}" "${sgRNAfile}" "${ref12}" 100 30 30
+$project_path/barcode/tools/barcode_align.sh "${fqR1}" 50 10 $(cat "${fqR1}.total")
+"${project_path}/barcode/tools/run_kpLogo.sh" "$project_path/barcode/test/A2-g1n-3.fq" weight
+"${project_path}/barcode/tools/trouble_shooting.r" "${fqR1}" -9999
 "${project_path}/barcode/tools/barcode_render.sh" "${project_path}/barcode/test/A2-g1n-3.fq" 50 10 60
--- barcode/tools/barcode_render.sh
+++ barcode/tools/barcode_render.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # Usage: barcode_render.sh fqR1 ext1up ext2up ref1len
 
+project_path="$(dirname $(realpath $0))/../.."
 fqR1=$1
 ext1up=$2
 ext2up=$3
@@ -9,6 +10,6 @@ algfile="${fqR1}.alg"
 tablefile="${fqR1}.table"
 sed -nr '/^[1-9]/,+2p; /^$/d' "${algfile}" >"${algfile}.${ext1up}.${ext2up}.${ref1len}"
 cut -f1 --complement ${tablefile} >"${tablefile}.${ext1up}.${ext2up}.${ref1len}"
-rearr_render.sh "${algfile}.${ext1up}.${ext2up}.${ref1len}"
+${project_path}/rearr_render.sh "${algfile}.${ext1up}.${ext2up}.${ref1len}"
 rm "${tablefile}.${ext1up}.${ext2up}.${ref1len}"
 rm "${algfile}.${ext1up}.${ext2up}.${ref1len}"
