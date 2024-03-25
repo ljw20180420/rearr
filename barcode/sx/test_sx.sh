50dd3e8ae60a50fc82d46bf165d5a5283badcc51
ljw <ljw2017@sjtu.edu.cn>
2024-03-25 18:28:55 +0800

    use local gawk-5.3.0/gawk instead of the system gawk

--- barcode/sx/test_sx.sh
+++ barcode/sx/test_sx.sh
@@ -7,10 +7,10 @@ spliter1="${csvfile}.adapter+sgRNA+scaffold"
 spliter2="${csvfile}.primer+barcode"
 sgRNAfile="${csvfile}.sgRNA"
 ref12="${csvfile}.ref12"
-read -ep "path to genome reference:" genomeref
+# read -ep "path to genome reference:" genomeref
 
-$project_path/barcode/sx/index_spliter.sh $genomeref $(ls ${project_path}/barcode/sx/csvfiles/*.csv)
-$project_path/barcode/tools/demultiplex.sh "${fqR1}" "${fqR2}" "${spliter1}" "${spliter2}" "${sgRNAfile}" "${ref12}" 100 30 30
+# $project_path/barcode/sx/index_spliter.sh $genomeref $(ls ${project_path}/barcode/sx/csvfiles/*.csv)
+# $project_path/barcode/tools/demultiplex.sh "${fqR1}" "${fqR2}" "${spliter1}" "${spliter2}" "${sgRNAfile}" "${ref12}" 100 30 30
 $project_path/barcode/tools/barcode_align.sh "${fqR1}" 50 10 $(cat "${fqR1}.total")
 "${project_path}/barcode/tools/run_kpLogo.sh" "$project_path/barcode/test/A2-g1n-3.fq" weight
 "${project_path}/barcode/tools/trouble_shooting.r" "${fqR1}" -9999
--- rearr_view.sh
+++ rearr_view.sh
@@ -3,9 +3,9 @@
 
 # display the first readnum (default: 50) read's alignments in ANSI format, the cut points and random insertions are aligned
 
+project_path="$(dirname $(realpath $0))"
 algfile=$1 # alignments (input.alg.cut.ext1.ext2)
 tablefile=$(sed -r 's/.alg./.table./' <<<"$algfile")
-read cut1 cut2 ref1len <<<"$(gawk -F "." -v OFS=" " '{print $(NF-2), $(NF-1), $NF}' <<<$algfile)"
+read cut1 cut2 ref1len <<<"$(${project_path}/gawk-5.3.0/gawk -F "." -v OFS=" " '{print $(NF-2), $(NF-1), $NF}' <<<$algfile)"
 readnum=${2:-50} # read number to display (default: 50)
-project_path="$(dirname $(realpath $0))"
-head -n$(("$readnum" * 3)) <"$algfile" | $project_path/tools/align_align.py "$ref1len" "$cut1" $(("$ref1len" + "$cut2")) | paste - <(head -n$(("$readnum" + 1)) "$tablefile" | gawk -F "\t" 'NR==1{for(i=1;i<=NF;++i) if($i=="percent"){pf=i;break} print ""} NR>1{printf("%s\n",$pf)}') | column -ts $'\t' | $project_path/less-643/less -SNR --header 1 --no-number-headers # view the first 50 (150/3) read alignments
+head -n$(("$readnum" * 3)) <"$algfile" | "${project_path}/tools/align_align.py" "$ref1len" "$cut1" $(("$ref1len" + "$cut2")) | paste - <(head -n$(("$readnum" + 1)) "$tablefile" | ${project_path}/gawk-5.3.0/gawk -F "\t" 'NR==1{for(i=1;i<=NF;++i) if($i=="percent"){pf=i;break} print ""} NR>1{printf("%s\n",$pf)}') | column -ts $'\t' | "${project_path}/less-643/less" -SNR --header 1 --no-number-headers # view the first 50 (150/3) read alignments
