aed8251eee998495c4fc49301768d73e25af5c52
ljw <ljw2017@sjtu.edu.cn>
2024-04-09 09:34:52 +0800

    add favicon.ico to flask, zip now does not include original fastq

--- flask_project/rearr_web.py
+++ flask_project/rearr_web.py
@@ -1,41 +1,45 @@
 #!/usr/bin/env python
 
 import os, uuid, subprocess, sys
-from flask import Flask, render_template, request, send_file, jsonify
+from flask import render_template, request, send_file, send_from_directory
 from werkzeug.utils import secure_filename
+from flaskApp import CustomFlask
+from celery_project.celery import celery_init_app
 
-# change Jinja2 delimiter to solve the comflicts with vue3
-class CustomFlask(Flask):
-    jinja_options = Flask.jinja_options.copy()
-    jinja_options.update(dict(
-        block_start_string='{b{s',
-        block_end_string='b}e}',
-        variable_start_string='{v{s',
-        variable_end_string='v}e}',
-        comment_start_string='{c{s',
-        comment_end_string='c}e}',
-    ))
+flaskApp = CustomFlask(__name__, static_folder="../vue_project/dist/assets", template_folder="../vue_project/dist", static_url_path="/assets")
 
-app = CustomFlask(__name__, static_folder="../vue_project/dist/assets", template_folder="../vue-project/dist", static_url_path="/assets")
+flaskApp.config.from_mapping(
+    CELERY=dict(
+        broker_url="amqp://",
+        result_backend="rpc://",
+    ),
+)
 
-@app.route("/")
+celeryApp = celery_init_app(flaskApp)
+
+@flaskApp.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(flaskApp.root_path, '../vue_project/dist'),
+                               'favicon.ico', mimetype='favicon.ico')
+
+@flaskApp.get("/")
 def home_page():
     return render_template("index.html")
 
-@app.route('/upload', methods = ["PUT"])
+@flaskApp.put('/upload')
 def uploadFile():
     file = request.files['files[]']
-    savepath = os.path.join(app.root_path, "jobs", uuid.uuid4().hex)
+    uUiD = uuid.uuid4().hex
+    savepath = os.path.join(flaskApp.root_path, "jobs", uUiD)
     os.makedirs(savepath, exist_ok=True)
     fastqfile = os.path.join(savepath, secure_filename(file.filename)) 
     file.save(fastqfile)
-    cmd = f'''{os.path.join(app.root_path, "../rearr_run.sh")} {fastqfile} {request.form['ref1']} {request.form['ref2']} {request.form['cut1']} {request.form['cut2']} {request.form['PAM1']} {request.form['PAM2']}'''
+    cmd = f'''{os.path.join(flaskApp.root_path, "../rearr_run.sh")} {fastqfile} {request.form['ref1']} {request.form['ref2']} {request.form['cut1']} {request.form['cut2']} {request.form['PAM1']} {request.form['PAM2']}'''
     with open(f"{fastqfile}.stdout", "w") as sdo, open(f"{fastqfile}.stderr", "w") as sde:
         _ = subprocess.run(cmd, stdout = sdo, stderr = sde, shell=True, executable="/bin/bash")
-    _ = subprocess.run(f'''zip -rj {savepath}.zip {savepath}''', shell=True, executable="/bin/bash")
+    _ = subprocess.run(f'''cd {os.path.join(flaskApp.root_path, "jobs")}; zip -rm {savepath}.zip {uUiD} -x {uUiD}/{file.filename}; rm -r {savepath}''', shell=True, executable="/bin/bash")
     return send_file(f'''{savepath}.zip''', as_attachment=True)
 
 if __name__ == "__main__":
-    print(app.root_path)
-    # app.run()
-    app.run(debug=True)
+    # flaskApp.run()
+    flaskApp.run(debug=True)
