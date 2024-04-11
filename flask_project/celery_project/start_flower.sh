d76cad5f524f587e8e52d504d4755115c0e26d65
ljw <ljw2017@sjtu.edu.cn>
2024-04-11 11:47:43 +0800

    run flower in docker

--- flask_project/celery_project/start_flower.sh
+++ flask_project/celery_project/start_flower.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
-flaskPath=$(dirname $(realpath $0))/..
-docker run -d --restart=always -v ${flaskPath}:/data -p 5555:5555 mher/flower celery --app=celery_project.app flower
+if ! docker ps | grep "celery_flower"
+then
+    if docker ps -a | grep "celery_flower"
+    then
+        docker container start celery_flower
+    else
+        flaskPath=$(dirname $(realpath $0))/..
+        docker run --name celery_flower -d --restart=always -v ${flaskPath}:/data -p 5555:5555 mher/flower celery --app=celery_project.app flower
+    fi
+fi
+
+
