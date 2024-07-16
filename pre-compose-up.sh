9e89ed35547bee0a72190f7c07e867fe2a0b354e
ljw <ljw2017@sjtu.edu.cn>
2024-07-16 16:44:02 +0800

    add chat-ui

--- pre-compose-up.sh
+++ pre-compose-up.sh
@@ -6,3 +6,7 @@ mkdir -p flask_project/tmp
 chmod a+w flask_project/tmp
 mkdir -p shinyApps/downstreamAnalysis/www
 chmod a+w shinyApps/downstreamAnalysis/www
+mkdir -p chat-ui/data/db
+chmod a+w chat-ui/data/db
+mkdir -p chat-ui/llama.cpp
+wget https://hf-mirror.com/microsoft/Phi-3-mini-4k-instruct-gguf/resolve/main/Phi-3-mini-4k-instruct-q4.gguf -O chat-ui/llama.cpp/Phi-3-mini-4k-instruct-q4.gguf
