eba106d56316985871b36b74e11569533e764ad2
ljw <ljw2017@sjtu.edu.cn>
2024-12-02 15:11:58 +0800

    add vue generated files to simplify flask image build

--- docker-images/shiny-server/apps/downstreamAnalysis/helpers/pairwise.R
+++
@@ -1,8 +0,0 @@
-pairwisePlot <- function(pairTibble, xscale, yscale, method, span) {
-    ggFig <- pairTibble |>
-        ggplot(aes(x = .data[[colnames(pairTibble)[1]]], y = .data[[colnames(pairTibble)[2]]])) +
-        geom_point() +
-        geom_smooth(method = method, span = span) +
-        scale_x_continuous(expand = c(0, 0), transform = xscale) +
-        scale_y_continuous(expand = c(0, 0), transform = yscale)
-}
--- setDockerProxy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-dhcpIp=$(hostname -I | cut -d' ' -f1)
-printf "[Service]\nEnvironment=\"HTTP_PROXY=socks5://%s:1081\"\nEnvironment=\"HTTPS_PROXY=socks5://%s:1081\"" $dhcpIp $dhcpIp >$HOME/.config/systemd/user/docker.service.d/http-proxy.conf
-systemctl --user daemon-reload
-systemctl --user restart docker
