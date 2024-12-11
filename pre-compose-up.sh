13cd051caf9fc108adfa9e171c7def34610721ff
ljw <ljw2017@sjtu.edu.cn>
2024-12-11 15:14:22 +0800

    add .dockerignore

--- pre-compose-up.sh
+++ pre-compose-up.sh
@@ -7,7 +7,6 @@ chmod a+w docker-images/shiny/apps/diffloopAnalysis/www
 chmod a+w docker-images/shiny/apps/diffloopAnalysisPair/www
 chmod a+w docker-images/shiny/apps/downstreamAnalysis/www
 chmod a+w docker-images/flask/flask_project/tmp
-mkdir -p chat-ui/data/db
-chmod a+w chat-ui/data/db
+chmod a+w docker-images/chat-ui/data/db
 mkdir -p chat-ui/llama.cpp
 wget https://hf-mirror.com/microsoft/Phi-3-mini-4k-instruct-gguf/resolve/main/Phi-3-mini-4k-instruct-q4.gguf -O chat-ui/llama.cpp/Phi-3-mini-4k-instruct-q4.gguf
