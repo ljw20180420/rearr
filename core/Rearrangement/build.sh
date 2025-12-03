82823678b151c0e7f9d1a5551abcd0e8af354f0a
ljw <ljw2017@sjtu.edu.cn>
2025-12-03 17:48:45 +0800

    Use build.sh to install rearrangement, which deletes old build directory before rebuild.

--- core/Rearrangement/build.sh
+++ core/Rearrangement/build.sh
@@ -3,6 +3,15 @@
 # Change directory to the script directory.
 cd $( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
+install_rearrangement=$1
+
+rm -r build
+mkdir -p build
 cd build
 cmake -DCMAKE_BUILD_TYPE=Release ..
 make
+
+if [ "${install_rearrangement}" = "install" ]
+then
+    make install
+fi
