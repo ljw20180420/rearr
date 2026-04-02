afc7c202554cb34009c1fb38cb160b32b938ee6e
ljw <ljw2017@sjtu.edu.cn>
2026-04-02 15:11:50 +0800

    Remove sx from install.sh

--- install.sh
+++ install.sh
@@ -15,16 +15,6 @@ cp core/removeDuplicates.sh ${prefix}/bin/
 # install demultiplex
 cp core/demultiplex/demultiplex.sh ${prefix}/bin/
 cp core/demultiplex/getAlignPos.awk ${prefix}/share/awk/
-# install getSxPlasmidFileRef
-cp sx/getSxPlasmidFileRef/getSxPlasmidFileRef.sh ${prefix}/bin/
-cp sx/getSxPlasmidFileRef/getSxPlasmidFileTarget.pl ${prefix}/bin/
-cp sx/getSxPlasmidFileRef/getSxRefFile.pl ${prefix}/bin/
-cp sx/getSxPlasmidFileRef/sxTargetSam2Bed.awk ${prefix}/share/awk/
-# install cutR2Adapter
-cp sx/sxCutR2AdapterFilterCumulate/sxCutR2AdapterFilterCumulate.sh ${prefix}/bin/
-cp sx/sxCutR2AdapterFilterCumulate/sxCumulateToMapCutAdaptMarker.awk ${prefix}/share/awk/
-# install sxExtractMarker
-cp sx/sxExtractMarker.sh ${prefix}/bin/
 # install runWorkFlow
 cp ./runWorkFlow.sh ${prefix}/bin/
 mkdir -p ${prefix}/share/rearr
